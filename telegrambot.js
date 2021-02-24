749aad02fc8489c95f00e374269348664308b3c8
Yash Agrawal <yashagrawal18@outlook.com>
2021-02-24 21:24:18 +0530

    Minor changes

---
+++ telegrambot.js
@@ -0,0 +1,38 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
+
+
+/**
+ * Send message to the telegram channel.
+ * @param {object} tradeDetails
+ */
+const sendUniswapSwapMessage = (tradeDetails) => {
+  const messageTemplate = `
+🚀 **${tradeDetails.action} ${Number(web3.utils.fromWei(tradeDetails.mainTokenAmount, 'ether')).toPrecision(6)} ${tradeDetails.mainTokenSymbol}  for ${Number(web3.utils.fromWei(tradeDetails.secondaryTokenAmount, 'ether')).toPrecision(6)} ${tradeDetails.secondaryTokenSymbol}  on Uniswap
+
+
+🟢  1 ${tradeDetails.mainTokenSymbol}   = ${tradeDetails.mainTokenPriceUSD || '-'}$ | ${tradeDetails.mainTokenPriceETH || '-'}ETH  🟢
+🟢  1 ${tradeDetails.secondaryTokenSymbol} = ${tradeDetails.secondaryTokenPriceUSD || '-'}$ | ${tradeDetails.secondaryTokenPriceETH || '-'}ETH  🟢
+
+
+📶 Tx 📶 [View](https://etherscan.io/tx/${tradeDetails.txHash})
+🦄 Uniswap 🦄 [View](https://info.uniswap.org/pair/${process.env.UNISWAP_PAIR_ADDRESS})
+
+
+Join $${tradeDetails.mainTokenSymbol} (${tradeDetails.mainTokenSymbol}) Telegram here (https://t.me/MahaDAO)
+  `
+
+  bot.sendMessage(
+    process.env.CHAT_ID,
+    messageTemplate,
+    { parse_mode: "Markdown" }
+  );
+}
+
+
+
+module.exports = {
+  bot
+}
--- uniswapSwap.js
+++ uniswapSwap.js
@@ -1,13 +1,12 @@
 const Web3 = require('web3');
 const Dagger = require('@maticnetwork/dagger');
-const TelegramBot = require('node-telegram-bot-api');
 
 require('dotenv').config();
+const { bot } = require('./telegrambot');
 const abi = require('./build/UniswapV2Pair.json').abi;
 const tokenAbi = require('./build/UniswapV2ERC20.json').abi;
 const { getCoinIdFromGecko, getPriceFromGecko } = require('./coingecko');
 
-const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
 const dagger = new Dagger("wss://mainnet.dagger.matic.network")
 const web3 = new Web3(`https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`);
 
@@ -19,13 +18,15 @@ const contract = dagger.contract(web3Contract);
  * Some state variables so that variable we need every now and then
  * are assigned and not around only once.
  */
-let token0 = null;
-let token1 = null;
-let symbol0 = null;
-let symbol1 = null;
-let token0GeckoId = null;
-let token1GeckoId = null;
-let isToken0ProtocolToken = false;
+const uniswapSwapGlobalState = {
+  token0 = null,
+  token1 = null,
+  symbol0 = null,
+  symbol1 = null,
+  token0GeckoId = null,
+  token1GeckoId = null,
+  isToken0ProtocolToken = false,
+}
 
 
 /**
@@ -33,54 +34,28 @@ let isToken0ProtocolToken = false;
  */
 const setTokenInfo = async () => {
   // Get the token0 and token1 for the pair.
-  token0 = await web3Contract.methods.token0().call();
-  token1 = await web3Contract.methods.token1().call();
+  uniswapSwapGlobalState.token0 = await web3Contract.methods.token0().call();
+  uniswapSwapGlobalState.token1 = await web3Contract.methods.token1().call();
 
   // Create a contract just to get the details info for each
   // of theset tokens.
   // NOTE: currently this contract is just used to extract the token
   // symbols.
-  const token0Contract = new web3.eth.Contract(tokenAbi, token0);
-  const token1Contract = new web3.eth.Contract(tokenAbi, token1);
+  const token0Contract = new web3.eth.Contract(tokenAbi, uniswapSwapGlobalState.token0);
+  const token1Contract = new web3.eth.Contract(tokenAbi, uniswapSwapGlobalState.token1);
 
   // Finally find the symbols of these tokens.
-  symbol0 = await token0Contract.methods.symbol().call();
-  symbol1 = await token1Contract.methods.symbol().call();
+  uniswapSwapGlobalState.symbol0 = await token0Contract.methods.symbol().call();
+  uniswapSwapGlobalState.symbol1 = await token1Contract.methods.symbol().call();
 
   // Check if protocol token is the same as token0 of the uniswap pair.
-  isToken0ProtocolToken = token0.toLowerCase() === process.env.PROTOCOL_TOKEN_ADDRESS.toLowerCase();
+  uniswapSwapGlobalState.isToken0ProtocolToken = (
+    uniswapSwapGlobalState.token0.toLowerCase() === process.env.PROTOCOL_TOKEN_ADDRESS.toLowerCase()
+  );
 
   // Set the coin gecko id for these particular tokens.
-  token0GeckoId = await getCoinIdFromGecko(symbol0);
-  token1GeckoId = await getCoinIdFromGecko(symbol1);
-}
-
-
-/**
- * Send message to the telegram channel.
- * @param {object} tradeDetails
- */
-const sendMessage = (tradeDetails) => {
-  const messageTemplate = `
-🚀 **${tradeDetails.action} ${Number(web3.utils.fromWei(tradeDetails.mainTokenAmount, 'ether')).toPrecision(6)} ${tradeDetails.mainTokenSymbol}  for ${Number(web3.utils.fromWei(tradeDetails.secondaryTokenAmount, 'ether')).toPrecision(6)} ${tradeDetails.secondaryTokenSymbol}  on Uniswap
-
-
-🟢  1 ${tradeDetails.mainTokenSymbol}   = ${tradeDetails.mainTokenPriceUSD || '-'}$ | ${tradeDetails.mainTokenPriceETH || '-'}ETH  🟢
-🟢  1 ${tradeDetails.secondaryTokenSymbol} = ${tradeDetails.secondaryTokenPriceUSD || '-'}$ | ${tradeDetails.secondaryTokenPriceETH || '-'}ETH  🟢
-
-
-📶 Tx 📶 [View](https://etherscan.io/tx/${tradeDetails.txHash})
-🦄 Uniswap 🦄 [View](https://info.uniswap.org/pair/${process.env.UNISWAP_PAIR_ADDRESS})
-
-
-Join $${tradeDetails.mainTokenSymbol} (${tradeDetails.mainTokenSymbol}) Telegram here (https://t.me/MahaDAO)
-  `
-
-  bot.sendMessage(
-    process.env.CHAT_ID,
-    messageTemplate,
-    { parse_mode: "Markdown" }
-  );
+  uniswapSwapGlobalState.token0GeckoId = await getCoinIdFromGecko(uniswapSwapGlobalState.symbol0);
+  uniswapSwapGlobalState.token1GeckoId = await getCoinIdFromGecko(uniswapSwapGlobalState.symbol1);
 }
 
 /**
@@ -93,16 +68,16 @@ const parseBotMessage = async (swap) => {
   let token0Amount = 0;
   let token1Amount = 0;
 
-  const price0USD = await getPriceFromGecko(token0GeckoId, 'usd');
-  const price0ETH = await getPriceFromGecko(token0GeckoId, 'eth');
+  const price0USD = await getPriceFromGecko(uniswapSwapGlobalState.token0GeckoId, 'usd');
+  const price0ETH = await getPriceFromGecko(uniswapSwapGlobalState.token0GeckoId, 'eth');
 
-  const price1USD = await getPriceFromGecko(token1GeckoId, 'usd');
-  const price1ETH = await getPriceFromGecko(token1GeckoId, 'eth');
+  const price1USD = await getPriceFromGecko(uniswapSwapGlobalState.token1GeckoId, 'usd');
+  const price1ETH = await getPriceFromGecko(uniswapSwapGlobalState.token1GeckoId, 'eth');
 
   let messageObj = null;
 
   // Check if token0 is our main protocol token or not.
-  if (isToken0ProtocolToken) {
+  if (uniswapSwapGlobalState.isToken0ProtocolToken) {
     // If yes, then check if we are buying or selling.
     // Accordingly set the amount.
     if (swap.returnValues.amount0Out == 0) {
@@ -123,11 +98,11 @@ const parseBotMessage = async (swap) => {
       mainTokenAmount: token0Amount,
       mainTokenPriceUSD: price0USD,
       mainTokenPriceETH: price0ETH,
-      mainTokenSymbol: symbol0,
+      mainTokenSymbol: uniswapSwapGlobalState.symbol0,
       secondaryTokenAmount: token1Amount,
       secondaryTokenPriceUSD: price1USD,
       secondaryTokenPriceETH: price1ETH,
-      secondaryTokenSymbol: symbol1
+      secondaryTokenSymbol: uniswapSwapGlobalState.symbol1
     }
   } else {
     // If no, then check again if we are buying or selling.
@@ -150,30 +125,20 @@ const parseBotMessage = async (swap) => {
       mainTokenAmount: token1Amount,
       mainTokenPriceUSD: price1USD,
       mainTokenPriceETH: price1ETH,
-      mainTokenSymbol: symbol1,
+      mainTokenSymbol: uniswapSwapGlobalState.symbol1,
       secondaryTokenAmount: token0Amount,
       secondaryTokenPriceUSD: price0USD,
       secondaryTokenPriceETH: price0ETH,
-      secondaryTokenSymbol: symbol0
+      secondaryTokenSymbol: uniswapSwapGlobalState.symbol0
     }
   }
 
-  sendMessage(messageObj);
+  sendUniswapSwapMessage(messageObj);
 }
 
 
 const main = async () => {
   await setTokenInfo();
-
-  bot.sendMessage(
-    process.env.CHAT_ID,
-    `
-
-    **BOT IS NOW LIVE! Get all the latest trades for the ** *${symbol0}-${symbol1}* ** pair from uniswap now!**
-
-    `,
-    { parse_mode: "Markdown" }
-  );
 
   const filter = contract.events.Swap({
     room: "latest"
