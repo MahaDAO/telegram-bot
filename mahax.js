d692b8d97e64cb8dfc4d69d1ec95ce6a1a51a90b
Rupali97 <rupalidoke97@gmail.com>
2022-02-21 21:22:04 +0530

    lock maha and loan message

--- mahax.js
+++ mahax.js
@@ -2,9 +2,13 @@ const Web3 = require('web3');
 const TelegramBot = require('node-telegram-bot-api');
 const twitter = require('twitter-lite');
 require('dotenv').config();
+const rp = require('request-promise');
+const moment =  require('moment')
 
 const config = require('./config');
-const abi = require('./VotingEscrow.json');
+const abi = require('./abi/VotingEscrow.json');
+const { messageTypes } = require('node-telegram-bot-api/src/telegram');
+const mahaImg = './MahaDAO.png'
 
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN)
 
@@ -13,38 +17,77 @@ const mahaXBot = async() => {
   const client = new twitter(config);
 
   // Polygon
-  const web3 = new Web3(process.env.MAINNET_MATIC)
-  var testContract = new web3.eth.Contract(abi, process.env.MAHA_CONTRACT_ADRS);
+  const web3 = new Web3(process.env.MAINNET_MATIC1)
+  var mahaxContract = new web3.eth.Contract(abi, '0x8F2C37D2F8AE7Bce07aa79c768CC03AB0E5ae9aE');
 
-  setInterval(() => {
-     testContract.events.Deposit({
-       filter: {
-         type: 2
-       }
-     })
-      .on('data', (event) => {
-        // if(event.event === "Deposit"){
-        //   console.log('Deposit Event', event);
-        //   let messageTemplate = `MAHA locked`
+  let mahaToUsdPrice = await rp(`https://api.coingecko.com/api/v3/simple/price?ids=mahadao&vs_currencies=usd`);
+  let mahaToEthPrice = await rp(`https://api.coingecko.com/api/v3/simple/price?ids=mahadao&vs_currencies=eth`);
 
-        // Telegram bot
-          // bot.sendMessage(
-          //   process.env.CHAT_ID,
-          //   messageTemplate,
-          //   { parse_mode: "Markdown" }
-          // );
-        // }
+  mahaToUsdPrice = Number(JSON.parse(mahaToUsdPrice)['mahadao']['usd']).toPrecision(4)
+  let ethToMahaPrice = Number(1 / JSON.parse(mahaToEthPrice)['mahadao']['eth']).toPrecision(6)
 
-        // client.post('statuses/update', { status: 'Hello investors..!' }).then(result => {
-        //   console.log('You successfully tweeted this : "' + result.text + '"');
-        // }).catch(console.error);
-        console.log('Data Event', event);
-      })
-      .on('changed', changed => console.log('changed', changed))
-      .on('connected', nr => {console.log('connected', nr)})
-      .on('error', err => console.log('error', err))
+  mahaxContract.events.allEvents({address: '0x8F2C37D2F8AE7Bce07aa79c768CC03AB0E5ae9aE'}, function(error, event){ })
+  .on('connected', nr => {console.log('connected', nr)})
+  .on('data', (event) => {
+    console.log('event', event);
+    let msgTemplate = '';
 
-    }, 5000)
+    if(event.event == 'Deposit' ){
+      if(event.returnValues.type == 1 || event.returnValues.type == 2){
+        let noOfGreenDots = Math.ceil(Number(event.returnValues.value / 10**18) / 0.001)
+
+        let greenDots = ''
+        for(let i = 0; i < noOfGreenDots; i++){
+          greenDots = '🟢 '  + greenDots;
+        }
+
+        msgTemplate = `
+        🚀  *${event.returnValues.value / 10**18} MAHA* has been locked till *${moment(event.returnValues.locktime * 1000).format('DD MMM YYYY')}* by [${event.returnValues.provider}](https://polygonscan.com/address/${event.returnValues.provider})
+
+${greenDots}
+
+*1 MAHA* = *$${mahaToUsdPrice}*
+*1 ETH* = *${ethToMahaPrice} MAHA*
+[MahaDAO](https://polygonscan.com/token/0xedd6ca8a4202d4a36611e2fff109648c4863ae19) *|* [📶 Tx Hash 📶 ](https://polygonscan.com/tx/${event.transactionHash})
+`
+      }else if(event.returnValues.type == 3){
+        msgTemplate = `
+        🚀  The locking period is extended till *${moment(event.returnValues.locktime * 1000).format('DD MMM YYYY')}* by [${event.returnValues.provider}](https://polygonscan.com/address/${event.returnValues.provider})
+
+*1 MAHA* = *$${mahaToUsdPrice}*
+*1 ETH* = *${ethToMahaPrice} MAHA*
+[MahaDAO](https://polygonscan.com/token/0xedd6ca8a4202d4a36611e2fff109648c4863ae19) *|* [📶 Tx Hash 📶 ](https://polygonscan.com/tx/${event.transactionHash})
+        `
+      }
+      else{
+        let noOfRedDots = Math.ceil(Number(event.returnValues.value / 10**18) / 0.001)
+
+        let redDots = ''
+        for(let i = 0; i < noOfRedDots; i++){
+          redDots = '🔴 '  + redDots;
+        }
+
+        msgTemplate = `
+        🚀  *${event.returnValues.value / 10**18} MAHA* has been withdrawn by [${event.returnValues.provider}](https://polygonscan.com/address/${event.returnValues.provider})
+
+*1 MAHA* = *$${mahaToUsdPrice}*
+*1 ETH* = *${ethToMahaPrice} MAHA*
+[MahaDAO](https://polygonscan.com/token/0xedd6ca8a4202d4a36611e2fff109648c4863ae19) *|* [📶 Tx Hash 📶 ](https://polygonscan.com/tx/${event.transactionHash})
+        `
+      }
+
+      bot.sendMessage(
+        process.env.CHAT_ID,
+        msgTemplate,
+        { parse_mode: "Markdown" }
+      );
+
+      // console.log('msgTemplate', msgTemplate);
+    }
+  })
+  .on('changed', changed => console.log('changed', changed))
+  .on('error', err => console.log('error', err))
+
 
 }
 mahaXBot();
